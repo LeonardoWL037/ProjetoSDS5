52c67bbaa344d86bf3a3b6decd9c113c95c01030
LeonardoWL037 <leonardo.wanderley10@hotmail.com>
2021-11-03 12:43:25 -0300

    Adicionada a paginação

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -1,34 +1,26 @@
-package com.devsuperior.dsvendas.entities;
+package com.devsuperior.dsvendas.dto;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import com.devsuperior.dsvendas.entities.Sale;
 
+public class SaleDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-@Entity
-@Table(name = "tb_sales")
-public class Sale {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
 	private Long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 	
-	@ManyToOne
-	@JoinColumn(name = "seller_id")
-	private Seller seller;
+	private SellerDTO seller;
+	
+	public SaleDTO() {
+	}
 
-	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
-		super();
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
 		this.id = id;
 		this.visited = visited;
 		this.deals = deals;
@@ -37,8 +29,13 @@ public class Sale {
 		this.seller = seller;
 	}
 	
-	public Sale() {
-		
+	public SaleDTO(Sale entity) {
+		this.id = entity.getId();
+		this.visited = entity.getVisited();
+		this.deals = entity.getDeals();
+		this.amount = entity.getAmount();
+		this.date = entity.getDate();
+		this.seller = new SellerDTO( entity.getSeller());
 	}
 
 	public Long getId() {
@@ -81,13 +78,19 @@ public class Sale {
 		this.date = date;
 	}
 
-	public Seller getSeller() {
+	public SellerDTO getSeller() {
 		return seller;
 	}
 
-	public void setSeller(Seller seller) {
+	public void setSeller(SellerDTO seller) {
 		this.seller = seller;
 	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
 	
 	
 }
